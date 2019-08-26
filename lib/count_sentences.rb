3162c70be5fa9bc92537e96f1dc267dcc6ef6409
Mars Le <hp.mars.le@gmail.com>
2019-08-26 20:38:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences #need to figure out with reg expressions
+    binding.pry
+    self.split(/(\!+\s?|\.+\s?|\?\s?)/).count
   end
 end
